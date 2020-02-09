18838617d7ed3a74aa9e6d36ece68b1c3d4bd3b9
sniklaus <simon.niklaus@outlook.com>
2020-02-09 11:54:56 -0800

    no message

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ except:
 
 ##########################################################
 
-assert(int(str('').join(torch.__version__.split('.')[0:3]).split('+')[0]) >= 41) # requires at least pytorch version 0.4.1
+assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 10) # requires at least pytorch version 1.0.0
 
 torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
 
