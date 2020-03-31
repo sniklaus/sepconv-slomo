474cf52556d72a77f07f59ad4951b0cb79ea9bc8
Simon Niklaus <simon.niklaus@outlook.com>
2020-03-31 09:54:27 -0700

    no message

--- download.bash
+++ download.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget --timestamping http://content.sniklaus.com/sepconv/network-l1.pytorch
-wget --timestamping http://content.sniklaus.com/sepconv/network-lf.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/sepconv/network-l1.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/sepconv/network-lf.pytorch
