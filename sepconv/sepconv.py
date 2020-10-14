33db3d0c27815146beee2b42e3ba7c2c1e5aa3f1
Simon Niklaus <simon.niklaus@outlook.com>
2020-10-13 21:46:04 -0700

    no message

--- sepconv/sepconv.py
+++ sepconv/sepconv.py
@@ -68,7 +68,7 @@ def cupy_kernel(strFunction, objVariables):
 	return strKernel
 # end
 
-@cupy.util.memoize(for_each_device=True)
+@cupy.memoize(for_each_device=True)
 def cupy_launch(strFunction, strKernel):
 	return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
 # end
